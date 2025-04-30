ae9995c77ea896a5bc4f54ac3a55a952c936dad5
Tommaso Visconti <tommaso.visconti@gmail.com>
2025-04-30 23:28:58 +0200

    organize ts imports

--- init.lua
+++ init.lua
@@ -90,7 +90,36 @@ require('lspconfig').ts_ls.setup({
   end,
 })
 
+require("typescript-tools").setup({
+  on_attach = function(client, bufnr)
+    local function organize_imports()
+      vim.lsp.buf.execute_command({
+        command = "_typescript.organizeImports",
+        arguments = { vim.api.nvim_buf_get_name(0) },
+        title = ""
+      })
+    end
+
+    -- Auto-command per ordinare import e rimuovere gli inutilizzati
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        organize_imports()
+        vim.lsp.buf.code_action({
+          context = { only = { "source.removeUnused" }, diagnostics = {} },
+          apply = true,
+        })
+      end,
+    })
+  end,
+})
+
+
 require("conform").setup({
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
   formatters_by_ft = {
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
