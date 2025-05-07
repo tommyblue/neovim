d15b81eac82571cd520ab395574bd833acdf0fe6
Tommaso Visconti <tommaso.visconti@gmail.com>
2025-05-07 14:50:23 +0200

    telescope-fzf: simplify build

--- lua/plugins/telescope-fzf.lua
+++ lua/plugins/telescope-fzf.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-telescope/telescope-fzf-native.nvim',
     dependencies = {'nvim-telescope/telescope.nvim'},
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+    --build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+    build = 'make'
 }
